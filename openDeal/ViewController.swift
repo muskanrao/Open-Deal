1073a4c02cbee00aaead78ac25dd6d7b2d82064a
muskanrao <muskan.rao4347@gmail.com>
2023-02-07 00:15:19 +0530

    Initial Commit

---
+++ openDeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openDeal
+//
+//  Created by Muskan on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
